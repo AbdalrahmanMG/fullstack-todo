9bf903dc9329ef32d88b965dba953f0373f9ee9f
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2024-07-09 22:36:52 +0300

    update & remove todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,15 +3,27 @@ import useAuthenticatedQuery from "../hooks/useAuthenticatedQuery";
 import Modal from "./ui/Modal";
 import Input from "./ui/Input";
 import { useState } from "react";
+import { ITodo } from "../interfaces";
+import Textarea from "./ui/Textarea";
+import axiosInstance from "../config/axios.config";
+import TodoSkeleton from "./TodoSkeleton";
 
 const TodoList = () => {
+  const defaultTodo = {
+    id: 0,
+    title: "",
+    description: "",
+  };
   const storageKey = "loggedInUser";
   const userDataString = localStorage.getItem(storageKey);
   const userData = userDataString ? JSON.parse(userDataString) : null;
   const [isEditModelOpen, setIsEditModelOpen] = useState(false);
+  const [istodoEditLoading, setIstodoEditLoading] = useState(false);
+  const [isOpenConfirmModal, setIsOpenConfirmModal] = useState(false);
+  const [todoToEdit, setTodoToEdit] = useState<ITodo>(defaultTodo);
 
   const { isLoading, data } = useAuthenticatedQuery({
-    queryKey: ["todos"],
+    queryKey: ["todoList", `${todoToEdit.id}`],
     url: "/users/me?populate=todos",
     config: {
       headers: {
@@ -21,23 +33,94 @@ const TodoList = () => {
   });
 
   //* handlers
-  const onToggleEditModel = () => {
-    setIsEditModelOpen((prev) => !prev);
+  const onOpenEditModel = (todo: ITodo) => {
+    setTodoToEdit(todo);
+    setIsEditModelOpen(true);
+  };
+  const onCloseEditModel = () => {
+    setTodoToEdit(defaultTodo);
+    setIsEditModelOpen(false);
+  };
+  const openConfirmModal = (todo: ITodo) => {
+    setTodoToEdit(todo);
+    setIsOpenConfirmModal(true);
+  };
+  const closeConfirmModal = () => {
+    setTodoToEdit(defaultTodo);
+    setIsOpenConfirmModal(false);
   };
 
-  if (isLoading) return <span>Loading...</span>;
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setTodoToEdit({
+      ...todoToEdit,
+      [name]: value,
+    });
+  };
+  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIstodoEditLoading(true);
+    try {
+      const { title, description } = todoToEdit;
+      const { status } = await axiosInstance.put(
+        `/todos/${todoToEdit.id}`,
+        { data: { title, description } },
+        {
+          headers: {
+            Authorization: `Bearer ${userData.jwt}`,
+          },
+        }
+      );
+      if (status === 200) {
+        onCloseEditModel();
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIstodoEditLoading(false);
+    }
+
+    console.log(todoToEdit);
+  };
+
+  const handleRemove = async () => {
+    setIstodoEditLoading(true);
+    try {
+      const { status } = await axiosInstance.delete(`/todos/${todoToEdit.id}`, {
+        headers: {
+          Authorization: `Bearer ${userData.jwt}`,
+        },
+      });
+      if (status === 200) {
+        closeConfirmModal();
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIstodoEditLoading(false);
+    }
+  };
+
+  if (isLoading)
+    return (
+      <div className="space-y-1 ">
+        {Array.from({length: 3},(_,idx)=>(
+          <TodoSkeleton key={idx}/>
+        ))}
+      </div>
+    );
 
   return (
     <div className="space-y-1 ">
       {data.todos.length ? (
-        data.todos.map((todo) => (
+        data.todos.map((todo: ITodo) => (
           <div key={todo.id} className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
             <p className="w-full font-semibold">1- {todo.title} </p>
             <div className="flex items-center justify-end w-full space-x-3">
-              <Button size={"sm"} onClick={onToggleEditModel}>
+              <Button size={"sm"} onClick={() => onOpenEditModel(todo)}>
                 Edit
               </Button>
-              <Button size={"sm"} variant={"danger"}>
+              <Button size={"sm"} variant={"danger"} onClick={() => openConfirmModal(todo)}>
                 Remove
               </Button>
             </div>
@@ -46,13 +129,33 @@ const TodoList = () => {
       ) : (
         <h3>No todos yet</h3>
       )}
-      <Modal title="Edit Todo" isOpen={isEditModelOpen} closeModal={onToggleEditModel}>
-        <Input />
-        <div className="flex items-center space-x-3 mt-4">
-          <Button size={"sm"} onClick={onToggleEditModel}>
-            Update
+
+      <Modal title="Edit Todo" isOpen={isEditModelOpen} closeModal={onCloseEditModel}>
+        <form onSubmit={submitHandler} className="space-y-3">
+          <Input name="title" value={todoToEdit.title} onChange={changeHandler} />
+          <Textarea name="description" value={todoToEdit.description} onChange={changeHandler} />
+          <div className="flex items-center space-x-3 mt-4">
+            <Button size={"sm"} isLoading={istodoEditLoading}>
+              Update
+            </Button>
+            <Button size={"sm"} variant={"cancel"} onClick={onCloseEditModel}>
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </Modal>
+
+      <Modal
+        title="Are you sure you want to remove this Todo from your Todos?"
+        isOpen={isOpenConfirmModal}
+        closeModal={closeConfirmModal}
+        description="Deleting this todo will remove it permanentaly from your todos list. Please make sure this is the intended action "
+      >
+        <div className="flex items-center space-x-3 mt-3">
+          <Button variant={"danger"} onClick={handleRemove} isLoading={istodoEditLoading}>
+            Yes, remove
           </Button>
-          <Button size={"sm"} variant={"cancel"} onClick={onToggleEditModel}>
+          <Button variant={"cancel"} onClick={closeConfirmModal}>
             Cancel
           </Button>
         </div>
--- src/components/TodoSkeleton.tsx
+++ src/components/TodoSkeleton.tsx
@@ -1,13 +1,13 @@
 const TodoSkeleton = () => {
   return (
-    <div className="flex items-center justify-between">
-      <div>
-        <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-      </div>
-      <div className="flex items-center space-x-2">
-        <div className=" w-20 h-9 bg-gray-300 rounded-md dark:bg-gray-700 w-12"></div>
-        <div className=" w-20 h-9 bg-gray-300 rounded-md dark:bg-gray-700 w-12"></div>
-      </div>
+    <div className="flex items-center justify-between p-3 rounded-md">
+        <div>
+            <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div className="flex items-center justify-end w-full space-x-3">
+        <div className="h-9 w-14 bg-gray-300 rounded-md dark:bg-gray-700"></div>
+        <div className="h-9 w-20 bg-gray-300 rounded-md dark:bg-gray-700"></div>
+        </div>
     </div>
   );
 };
